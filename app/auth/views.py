73c2775392bf84fd0580e3555c9bc1787df561ff
Tony <tr33947@gmail.com>
2020-12-17 12:43:28 +0300

    made changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,7 +28,7 @@ def signup():
     form = RegForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
-        user.save_u()
+        user.save()
         mail_message("Welcome to Daily_Blogs","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     return render_template('auth/signup.html', r_form = form)
